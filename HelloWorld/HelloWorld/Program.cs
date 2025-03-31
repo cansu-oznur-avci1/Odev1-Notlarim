573e983146aaa4cd66b6c16f4cfccc760f0aff6d
Cansu Oznur AVCI <canozavci0203@gmail.com>
2025-03-31 18:33:25 +0300

    Yeni dosya eklendi

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); //Bunu yazınca (sadece) çalıştırıp bitiriyor 
+            Console.Write("Yeni satır"); //New line eklemeyecek
+            Console.Write("Diğer satır"); //Aynı satırda yazmaya devam eder
+            Console.ReadLine(); //ilk yorum satırı + O yüzden ayrıca bu satırı da yazdık
+
+            //'' kullanınca tek karakter yazabilirim
+            //"" kullanınca birden fazla karakter yazabilirim
+
+        }
+    }
+}
